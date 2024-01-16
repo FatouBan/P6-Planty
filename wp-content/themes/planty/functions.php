422025e5831a9c65a7e9d72ccee2a71ab9847026
Fatou Ban <sekimi.fatma@gmail.com>
2024-01-16 15:04:31 +0100

    Ajout de code

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -1,9 +1,35 @@
 <?php
+// Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css',);
+    // Chargement du style.css
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', 
+    array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+}
+
+
+/*** Hook Filter  ***/
+add_filter('show_admin_bar', '__return_false');
+
+add_filter('wp_nav_menu_items', 'admin_link', 10, 2);
+
+
+function admin_link($items, $args)
+{
+
+    if (is_user_logged_in()) {
+        $adminlink = admin_url();
+        $items .= '<li class="menu-item"><a href="' . $adminlink . '">Admin</a></li>';
+        $items .= '<li class="menu-commander"><a href="' . site_url('/commande') . '">Commander</a></li>';
+        return $items;
+
+    } else {
+        $items .= '<li class="menu-commander"><a href="' . site_url('/commande') . '">Commander</a></li>';
+        return $items;
+    }
+
 }
 
 ?>
